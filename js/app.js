81ab6eaf8a7e29fa923b62dcf848ca2a345f9f4d
Stevey Z <sjzerneri@gmail.com>
2016-01-06 21:13:50 -0500

    adding keycode functionality

--- js/app.js
+++ js/app.js
@@ -1,43 +1,64 @@
 $(document).ready(function () {
+
+    /*on page load —> display the default state --> show ryu-still (and hide everythying else)*/
+    $('.ryu-action').hide(); //(this line means hide all 4 divs)
+    /* is replacing ...
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').hide();
+    */
+    $('.ryu-still').show();
+
+    /*  on mouse over
+                    -> mouse enter -> display ryu-ready (and hide everything else)*/
     $('.ryu').mouseenter(function () {
-            $('.ryu-still').hide();
-            $('.ryu-ready').show();
-        })
-        .mouseleave(function () {
-            $('.ryu-still').show();
-            $('.ryu-ready').hide();
-        })
-        .mousedown(function () {
-            playHadouken();
-            $('.ryu-ready').hide();
-            $('.ryu-throwing').show();
-            $('.hadouken').finish().show().animate({
-                    'left': '1020px'
-                },
-                500,
-                function () {
-                    $(this).hide();
-                    $(this).css('left', '520px');
-                });
-        })
-        .mouseup(function () {
-            $('.ryu-throwing').hide();
-            $('.ryu-ready').show();
-        });
+        $('.ryu-action').hide(); //this line means hide all 4 divs
+        $('.ryu-ready').show();
+    });
+    $('.ryu').mouseleave(function () {
+        $('.ryu-action').hide();
+        $('.ryu-still').show();
+    });
+    /*on click
+            —> mousedown —> display ryu-throwing (and hide everythying else)*/
+    $('.ryu').mousedown(function () {
+        playHadouken();
+        $('.ryu-action').hide(); //(this line means hide all 4 divs)
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate({
+                'left': '1020px'
+            },
+            500,
+            function () {
+                $(this).stop();
+                $(this).hide();
+                $(this).css('left', '520px');
+            }
+        );
+    });
+
+    /*    —> mouseup —> display the default state --> ryu-still (and hide everythying else)*/
+    $('.ryu').mouseup(function () {
+        $('.ryu-action').hide(); //(this line means hide all 4 divs)
+        $('.ryu-still').show();
+    });
+
+
     //x key functionality when mouse is outside ryu div
 
-    $('html').keydown(function (e) {
-            if (e.keyCode == 88) {
-                $('.ryu-still').hide();
-                $('.ryu-cool').show();
-            }
-        })
-        .keyup(function (e) {
-            if (e.keyCode == 88) {
-                $('.ryu-cool').hide();
-                $('.ryu-still').show();
-            }
-        });
+    $(document).keydown(function (e) {
+        if (e.keyCode == 88) {
+            $('.ryu-action').hide();
+            $('.ryu-cool').show();
+        }
+    });
+    $(document).keyup(function (e) {
+        if (e.keyCode == 88) {
+            $('.ryu-action').hide();
+            $('.ryu-still').show();
+        }
+    });
 
 });
 
